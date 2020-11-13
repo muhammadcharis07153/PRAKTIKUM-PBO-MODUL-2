e3a150e9222bb73e3b19b6dcf4f6bf9def5b53a5
adelarizky96 <74408380+adelarizky96@users.noreply.github.com>
2020-11-13 18:17:48 +0700

    Create Adela07128_Kendaraan.java

---
+++ .github/workflows/Adela07128_Kendaraan.java
@@ -0,0 +1,21 @@
+package apkparkir;
+
+
+public class Adela07128_Kendaraan {
+    String adela07128_platnomor;
+    String adela07128_jeniskendaraan;
+ 
+    public Adela07128_Kendaraan(String adela07128_platnomor,String adela07128_jeniskendaraan){
+        this.adela07128_platnomor = adela07128_platnomor;
+        this.adela07128_jeniskendaraan = adela07128_jeniskendaraan;
+    }
+    
+    String getadela07128_PlatNomor() {
+        return this.adela07128_platnomor;
+    }
+    
+    String getadela07128_JenisKendaraan() {
+        return this.adela07128_jeniskendaraan;
+    }   
+
+}
